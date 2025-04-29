d9ec0b79f865c71ecb098b8ed0a84130e5e1b1d5
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-04-30 00:41:32 +0300

    now returns all modifiers

--- src/modifiers.ts
+++ src/modifiers.ts
@@ -4,8 +4,6 @@ import {
     InsertTextFormat,
 } from "vscode-languageserver";
 
-type ModifierType = "keyboard" | "mouse" | "behavior";
-
 const behaviorModifiers = [
     "prevent",
     "stop",
@@ -47,75 +45,30 @@ const keyboardModifiers = [
 
 const mouseModifiers = ["shift", "ctrl", "alt", "meta", "cmd"];
 
-const modifiers = (type: ModifierType): CompletionItem[] => {
-    if (type === "mouse") {
-        return [
-            ...mouseModifiers.map((modifier) => ({
-                label: "[KEY] " + modifier,
-                kind: CompletionItemKind.EnumMember,
-                insertText: modifier,
-                insertTextFormat: InsertTextFormat.PlainText,
-                data: {
-                    type: "mouseModifier",
-                    value: modifier,
-                },
-                documentation: `Mouse modifier for Alpine.js. Use with x-on: or @ directives.`,
-            })),
-            ...modifiers("behavior"),
-        ];
-    }
-
-    if (type === "behavior") {
-        return behaviorModifiers.map((modifier) => ({
+const modifiers = (): CompletionItem[] => {
+    return [
+        ...mouseModifiers.map((modifier) => ({
+            label: "[KEY] " + modifier,
+            kind: CompletionItemKind.EnumMember,
+            insertText: modifier,
+            insertTextFormat: InsertTextFormat.PlainText,
+            documentation: `Mouse modifier for Alpine.js. Use with x-on: or @ directives.`,
+        })),
+        ...behaviorModifiers.map((modifier) => ({
             label: modifier,
             kind: CompletionItemKind.EnumMember,
             insertText: modifier,
             insertTextFormat: InsertTextFormat.PlainText,
-            data: {
-                type: "behaviorModifier",
-                value: modifier,
-            },
             documentation: `Behavior modifier for Alpine.js. Use with x-on: or @ directives.`,
-        }));
-    }
-
-    return [
+        })),
         ...keyboardModifiers.map((modifier) => ({
             label: "[KEY] " + modifier,
             kind: CompletionItemKind.EnumMember,
             insertText: modifier,
             insertTextFormat: InsertTextFormat.PlainText,
-            data: {
-                type: "keyModifier",
-                value: modifier,
-            },
             documentation: `Key modifier for Alpine.js. Use with x-on: or @ directives.`,
         })),
-        ...modifiers("behavior"),
     ];
 };
 
-function rawModifiers(type: ModifierType): string[] {
-    if (type === "mouse") {
-        return mouseModifiers;
-    }
-
-    if (type === "behavior") {
-        return behaviorModifiers;
-    }
-
-    if (type === "keyboard") {
-        return keyboardModifiers;
-    }
-
-    return [];
-}
-
-export {
-    behaviorModifiers,
-    keyboardModifiers,
-    mouseModifiers,
-    modifiers,
-    rawModifiers,
-};
-export type { ModifierType as AlpineModifierType };
+export { behaviorModifiers, keyboardModifiers, mouseModifiers, modifiers };
