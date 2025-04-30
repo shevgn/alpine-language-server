0362a852cde94ded09a461d5227d48987324bb6c
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-04-30 22:47:05 +0300

    ts lsp moved to file

---
+++ src/typescript-server.ts
@@ -0,0 +1,59 @@
+import { spawn, ChildProcess } from "child_process";
+import {
+    CompletionParams,
+    createProtocolConnection,
+    InitializedNotification,
+    InitializeRequest,
+    ProtocolConnection,
+    StreamMessageReader,
+    StreamMessageWriter,
+} from "vscode-languageserver/node.js";
+
+let tsConnection: ProtocolConnection | null = null;
+let alpineUri: string | null = null;
+
+async function setupTypescriptServer(): Promise<ProtocolConnection> {
+    const tsProcess: ChildProcess = spawn("typescript-language-server", [
+        "--stdio",
+    ]);
+
+    tsConnection = createProtocolConnection(
+        new StreamMessageReader(tsProcess.stdout!),
+        new StreamMessageWriter(tsProcess.stdin!)
+    );
+
+    tsConnection.listen();
+
+    const initResult = await tsConnection.sendRequest(
+        InitializeRequest.method,
+        {
+            processId: process.pid,
+            rootUri: null,
+            capabilities: {},
+        }
+    );
+
+    tsConnection.sendNotification(InitializedNotification.method);
+
+    return tsConnection;
+}
+
+function openAlpineContext(params: CompletionParams): void {
+    const fileUri = params.textDocument.uri;
+    alpineUri = `inmemory://model/${fileUri}.alpine.js`;
+
+    tsConnection!.sendNotification("textDocument/didOpen", {
+        textDocument: {
+            uri: alpineUri,
+            languageId: "javascript",
+            version: 1,
+            text: "",
+        },
+    });
+}
+
+function getAlpineUri(): string {
+    return alpineUri!;
+}
+
+export { setupTypescriptServer, getAlpineUri, openAlpineContext };
