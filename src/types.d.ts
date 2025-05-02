91557ca6fbe87a0064bae4cd5e8b40f55ddf6239
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-05-02 15:40:31 +0300

    types definitions

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+export type XDataProps = {
+    name: string;
+    kind: CompletionItemKind;
+};
+
+export type XDataTag = {
+    tag: string;
+    xData: string;
+    range: { start: number; end: number };
+};
