c6c20c519183518c425ba118f11b080880238d39
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-05-05 17:18:11 +0300

    directives as list

--- src/directives.ts
+++ src/directives.ts
@@ -4,72 +4,60 @@ import {
     InsertTextFormat,
 } from "vscode-languageserver";
 
+type Directive = {
+    name: string;
+    description: string;
+    insertText?: string;
+};
+
+const directivesList: Directive[] = [
+    {
+        name: "x-data",
+        description:
+            "Defines a chunk of HTML as an Alpine component and provides the reactive data for that component to reference.",
+    },
+    {
+        name: "x-init",
+        description:
+            "Allows you to hook into the initialization phase of any element in Alpine.",
+    },
+    {
+        name: "x-show",
+        description:
+            "Allows you to conditionally show or hide an element based on a boolean value.",
+    },
+    {
+        name: "x-bind",
+        description: "Allows you to bind HTML attributes to Alpine data.",
+        insertText: 'x-bind:${1}="${2}"',
+    },
+    {
+        name: "x-on",
+        description: "Allows you to easily run code on dispatched DOM events.",
+        insertText: 'x-on:${1}="${2}"',
+    },
+    {
+        name: "x-text",
+        description:
+            "Allows you to set the text content of an element to the result of a given expression.",
+    },
+    {
+        name: "x-html",
+        description:
+            "Allows you to set the innerHTML property of an element to the result of a given expression.",
+    },
+];
+
 const directives = (): CompletionItem[] => {
-    return [
-        {
-            label: "x-data",
-            kind: CompletionItemKind.Keyword,
-            insertText: 'x-data="${1}"',
-            insertTextFormat: InsertTextFormat.Snippet,
-            documentation: `
-Defines a chunk of HTML as an Alpine component and provides the reactive data for that component to reference.
-`,
-        },
-        {
-            label: "x-init",
-            kind: CompletionItemKind.Keyword,
-            insertText: 'x-init="${1}"',
-            insertTextFormat: InsertTextFormat.Snippet,
-            documentation: `
-Allows you to hook into the initialization phase of any element in Alpine.
-            `,
-        },
-        {
-            label: "x-show",
-            kind: CompletionItemKind.Keyword,
-            insertText: 'x-show="${1}"',
-            insertTextFormat: InsertTextFormat.Snippet,
-            documentation: `
-Allows you to conditionally show or hide an element based on a boolean value.
-            `,
-        },
-        {
-            label: "x-bind",
-            kind: CompletionItemKind.Keyword,
-            insertText: "x-bind:${1}",
-            insertTextFormat: InsertTextFormat.Snippet,
-            documentation: `
-Allows you to set HTML attributes on elements based on the result of JavaScript expressions.
-            `,
-        },
-        {
-            label: "x-on",
-            kind: CompletionItemKind.Keyword,
-            insertText: "x-on:${1}",
-            insertTextFormat: InsertTextFormat.Snippet,
-            documentation: `
-Allows you to easily run code on dispatched DOM events.
-`,
-        },
-        {
-            label: "x-text",
-            kind: CompletionItemKind.Keyword,
-            insertText: 'x-text="${1}"',
-            insertTextFormat: InsertTextFormat.Snippet,
-            documentation: `
-Sets the text content of an element to the result of a given expression.
-`,
-        },
-        {
-            label: "x-html",
-            kind: CompletionItemKind.Keyword,
-            insertText: 'x-html="${1}"',
-            insertTextFormat: InsertTextFormat.Snippet,
-            documentation: `
-Sets the "innerHTML" property of an element to the result of a given expression.
-`,
-        },
-    ];
+    return directivesList.map((directive) => ({
+        label: directive.name,
+        kind: CompletionItemKind.Keyword,
+        documentation: directive.description,
+        insertText: directive.insertText
+            ? directive.insertText
+            : directive.name + '="${1}"',
+        insertTextFormat: InsertTextFormat.Snippet,
+    }));
 };
 
 export { directives };
